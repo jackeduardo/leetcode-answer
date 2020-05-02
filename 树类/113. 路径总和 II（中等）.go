953073ff3e1de0ebe03a200c38dc42c688af5eac
jackeduardo <xuhaiyang1996113@gmail.com>
2020-05-02 17:06:31 -0500

    124

---
+++ 树类/113. 路径总和 II（中等）.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"leetcode-answer/Methods"
+	"leetcode-answer/Types"
+)
+
+func main() {
+	arr:="[5,4,8,11,null,13,4,7,2,null,null,5,1]"
+	root:=Methods.CreateTree(arr)
+	sum:=22
+	fmt.Println(pathSum(root,sum))
+}
+
+func pathSum(root * Types.TreeNode, sum int) [][]int {
+	var res [][]int
+	var temp []int
+	pathSum_helper(root,sum,&res,&temp)
+	return res
+}
+// 类似回溯，一层函数的功能，再找递归位置
+func pathSum_helper(root * Types.TreeNode, sum int, res*[][]int,temp *[]int){
+	if root==nil{
+		return
+	}
+	sum-=root.Val
+	*temp=append(*temp,root.Val)
+	if root.Left==nil&&root.Right==nil{
+		if sum==0{
+			out:=make([]int,len(*temp))
+			copy(out,*temp)
+			*res=append(*res,out)
+		}
+	}
+	pathSum_helper(root.Left,sum,res,temp)
+	pathSum_helper(root.Right,sum,res,temp)
+	*temp=(*temp)[:len(*temp)-1]
+}
