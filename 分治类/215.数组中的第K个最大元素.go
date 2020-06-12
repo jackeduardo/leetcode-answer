7fc2753df85e817efb2dfadb90dc23228680ebd0
xuhaiyang <xuhaiyang@bilibili.com>
2020-06-12 06:47:43 -0500

    daily update

--- 分治类/215.数组中的第K个最大元素.go
+++ 分治类/215.数组中的第K个最大元素.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"math/rand"
 	"time"
 )
 
-//最简单的方法就是调用sort库函数，但是这里是训练使用分治法
+//最简单的方法就是调用sort库函数，但是这里是训练使用分治法或者利用最大堆，但是golang没有相关实现的小顶堆，只能够自己实现，比较麻烦
 //分治的核心就是找到最小单位进行按需计算
 func main() {
 	nums := []int{3, 2, 1, 5, 6, 4}
-	fmt.Print(findKthLargest(nums, 2))
+	fmt.Print(findKthLargest2(nums, 2))
 
 }
 
@@ -56,4 +57,44 @@ func partition(nums []int, left int, right int) int {
 	}
 	nums[left], nums[rt] = nums[rt], nums[left]
 	return rt
+}
+
+
+//堆排序的实现，摘抄
+type TopList []int
+
+func (t TopList)Len()int{
+	return len(t)
+}
+func (t TopList)Swap(i,j int){
+	t[i],t[j] = t[j],t[i]
+}
+func (t TopList)Less(i,j int)bool{
+	return  t[i]<t[j]
+}
+func (t *TopList)Push(x interface{}){
+	*t = append(*t,x.(int))
+}
+func (t *TopList)Pop()interface{}{
+	x := (*t)[len(*t)-1]
+	*t= (*t)[:len(*t)-1]
+	return x
+}
+
+
+func findKthLargest2(nums []int, k int) int {
+	m :=make(TopList,0)
+	size :=0
+	for i:=range nums{
+		if size<k{
+			heap.Push(&m,nums[i])
+			size++
+		}else {
+			if nums[i]>m[0]{//小顶堆 堆顶元素小于当前元素
+				heap.Pop(&m)//必须加&，因为实现的接口中 func (t *TopList)Push(x interface{})对应的是*toplist，是一个指针
+				heap.Push(&m,nums[i])
+			}
+		}
+	}
+	return m[0]
 }
---
+++ 堆类/253. 会议室 II（中等）.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	intervals:=[][]int{
+		{13,15},
+		{1,13},
+	}
+	fmt.Println(minMeetingRooms2(intervals))
+}
+
+//贪心解法
+func minMeetingRooms(intervals [][]int) int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0]<intervals[j][0]
+	})
+	for i := 0; i <len(intervals); i++ {
+		for j := i+1; j <len(intervals); j++ {
+			if intervals[i][1]<=intervals[j][0]{
+				intervals[i][1]=intervals[j][1]
+				intervals = append(intervals[:j], intervals[j+1:]...)
+				j--
+			}
+		}
+	}
+	return len(intervals)
+}
+//上下车算法
+
+func minMeetingRooms2(intervals [][]int) int {
+	var nums [][]int
+	for _, v := range intervals {
+		nums = append(nums, []int{v[0],1})//上车是1（进会议室）
+		nums = append(nums, []int{v[1],0})//下车是0（出会议室）
+	}
+	sort.Slice(nums, func(i, j int) bool {
+		if nums[i][0]==nums[j][0]{
+			return nums[i][1]<nums[j][1]//先出后进，防止多算
+		}
+		return nums[i][0]<nums[j][0]
+	})
+	maxRoom := 0
+	curNeedRoom := 0
+	for _, v := range nums {
+		if v[1]== 1 {
+			curNeedRoom++
+		} else {
+			curNeedRoom--
+		}
+		if curNeedRoom > maxRoom {
+			maxRoom = curNeedRoom
+		}
+	}
+	return maxRoom
+
+}
